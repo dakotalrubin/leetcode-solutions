76cb4fe61aa964111065d5776e708f23164c20ee
Dakota Rubin <dakotalrubin@gmail.com>
2024-01-17 10:14:24 -0800

    Time: 62 ms (89.68%), Space: 17.9 MB (7.02%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -0,0 +1,31 @@
+# The solution has a time complexity of O(n), since you need to iterate
+# through the input array once. The space complexity is O(n), since I'm
+# using a stack to store expressions.
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        # Create a stack to push and pop expressions from the input array
+        stack = []
+
+        for token in tokens:
+            if token == "+":
+                # Add the two most recent expressions
+                stack.append(stack.pop() + stack.pop())
+            elif token == "-":
+                # Subtract the two most recent expressions in the order
+                # they were pushed onto the stack
+                b, a = stack.pop(), stack.pop()
+                stack.append(a - b)
+            elif token == "*":
+                # Multiply the two most recent expressions
+                stack.append(stack.pop() * stack.pop())
+            elif token == "/":
+                # Divide the two most recent expressions in the order
+                # they were pushed onto the stack
+                b, a = stack.pop(), stack.pop()
+                stack.append(int(a / b))
+            else:
+                # Push an expression onto the stack
+                stack.append(int(token))
+
+        # Return the evaulated expression
+        return stack.pop()
