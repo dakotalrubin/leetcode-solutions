257fd063c30877011e447ed4708b5ac8b4a92b2b
Dakota Rubin <dakotalrubin@gmail.com>
2024-01-18 09:29:50 -0800

    Update 0022-generate-parentheses.py

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,9 +1,13 @@
+# The solution has a time complexity of O(V+E), where V is the number
+# of vertices (possible combinations of well-formed parentheses) and
+# E is the number of edges between vertices. The space complexity is
+# O(V), since the graph could be a straight line in the worst case.
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         # Perform a depth-first search to find all possible
         # combinations of well-formed parentheses
         def backtrack(openN, closedN, path):
-            # The max numbers of opening and closing parentheses
+            # The maximum numbers of opening and closing parentheses
             # have been used
             if openN == closedN == n:
                 paths.append(path)
@@ -13,7 +17,7 @@ class Solution:
             if openN < n:
                 backtrack(openN + 1, closedN, path + "(")
 
-            # Add a closed parenthesis to the path
+            # Add a closing parenthesis to the path
             if closedN < openN:
                 backtrack(openN, closedN + 1, path + ")")
 
@@ -21,7 +25,7 @@ class Solution:
         # and closing parentheses
         openN = closedN = 0
 
-        # Create a path variable to represent one possible combination
+        # Create a path string to represent one possible combination
         # of well-formed parentheses
         path = ""
 
